4cb8cd5e23165c344cd260940399b05a4b48d3b0
Abhranil Bhattacharjee <abhra.tb@gmail.com>
2020-05-10 20:47:46 +0530

    Find the Town Judge - Faster Solution

--- May Leetcode Challenge/townJudge.java
+++ May Leetcode Challenge/townJudge.java
@@ -2,28 +2,55 @@ class Solution {
     
     public int findJudge(int N, int[][] trust) {
         
-        boolean trustsSomeone[]=new boolean[N+1];
+        /* HASHSET SOLUTION
         
-        HashSet[] dp=new HashSet[N+1];
+                boolean trustsSomeone[]=new boolean[N+1];
+
+                HashSet[] dp=new HashSet[N+1];
+
+                for(int i=0;i<=N;i++)
+                    dp[i]=new HashSet<Integer>();
+
+                int n=trust.length;
+                for(int i=0;i<n;i++)
+                {
+                    int a=trust[i][0];
+                    int b=trust[i][1];
+                    dp[b].add(a);
+                    trustsSomeone[a]=true;
+
+                }
+
+
+                int answer=-1;
+                for(int i=1;i<=N;i++)
+                {
+                    if(dp[i].size()==(N-1) && trustsSomeone[i]==false)
+                    {
+                        if(answer==-1)
+                            answer=i;
+                        else
+                            return -1;
+                    }
+                }
+                return answer;
+        */
         
-        for(int i=0;i<=N;i++)
-            dp[i]=new HashSet<Integer>();
-        
-        int n=trust.length;
-        for(int i=0;i<n;i++)
+        int numberOfPersonTrusts[] = new int[N+1];
+        boolean trustsSomeone[] = new boolean[N+1];
+        for(int i=0;i<trust.length;i++)
         {
             int a=trust[i][0];
             int b=trust[i][1];
-            dp[b].add(a);
             trustsSomeone[a]=true;
-            
+            numberOfPersonTrusts[b]++;
+
         }
-        
         
         int answer=-1;
         for(int i=1;i<=N;i++)
         {
-            if(dp[i].size()==(N-1) && trustsSomeone[i]==false)
+            if(numberOfPersonTrusts[i]==(N-1) && trustsSomeone[i]==false)
             {
                 if(answer==-1)
                     answer=i;
@@ -31,7 +58,6 @@ class Solution {
                     return -1;
             }
         }
-        return answer;
-        
+        return answer;        
     }
 }
