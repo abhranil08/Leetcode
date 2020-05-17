c66f9ee21703a68d774d24cf076c3689071317fa
Abhranil Bhattacharjee <abhra.tb@gmail.com>
2020-05-17 23:03:01 +0530

    Find All Anagrams in a String - Update

--- May Leetcode Challenge/findAnagramsInAString.java
+++ May Leetcode Challenge/findAnagramsInAString.java
@@ -14,17 +14,46 @@ class findAnagramsInAString {
                 return false;
         return true;
     }
+    
     public List<Integer> findAnagrams(String s, String p) {
         int n=p.length();
         
         int freqP[]=frequencyArray(p);
         List<Integer> output=new ArrayList<>();
         
-        for(int i=0;i<s.length()-n+1;i++)
+        /*for(int i=0;i<s.length()-n+1;i++)
         {
             String substr=s.substring(i,i+n);
             if(isSameArray(freqP,frequencyArray(substr)))
                 output.add(i);
+        }*/
+        
+        if(p.length()<=s.length())
+        {
+            int freqS[]=new int[26];
+            for(int i=0;i<p.length();i++)
+                freqS[(int)s.charAt(i)-'a']++;
+            
+            //int freqS[] = frequencyArray(s.substring(0,p.length()));
+            
+            if(isSameArray(freqS,freqP))
+                output.add(0);
+            
+            for(int i=p.length();i<s.length();i++)
+            {
+                char ch1 = s.charAt(i);
+                char ch2 = s.charAt(i-p.length());
+                
+                if(ch1!=ch2)
+                {
+                    freqS[(int)ch2-'a']--;
+                    freqS[(int)ch1-'a']++;
+                }
+                
+                if(isSameArray(freqS,freqP))
+                    output.add(i-p.length()+1);
+            }
+                
         }
         return output;
         
