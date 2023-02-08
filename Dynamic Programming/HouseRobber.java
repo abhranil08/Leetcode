d6b1bd8da611d10a4a586bf4fe0c1bd1a68d8f7d
Abhranil Bhattacharjee <abhra.tb@gmail.com>
2023-02-08 06:10:03 +0530

    House Robber
    You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
    
    Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
    
     
    
    Example 1:
    
    Input: nums = [1,2,3,1]
    Output: 4
    Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
    Total amount you can rob = 1 + 3 = 4.
    Example 2:
    
    Input: nums = [2,7,9,3,1]
    Output: 12
    Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
    Total amount you can rob = 2 + 9 + 1 = 12.

---
+++ Dynamic Programming/HouseRobber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        if( nums.length == 1)
+            return nums[0];
+
+        int dp[] = new int[nums.length];
+        dp[0] = nums[0]; dp[1] = nums[1];
+
+        if(nums.length > 2 ) 
+            dp[2] = Math.max((nums[0] + nums[2]), nums[1]);
+
+        for( int i = 3;i<nums.length;i++)
+        {
+            dp[i] = nums[i]+ Math.max( dp[i-2], dp[i-3] );
+        }
+        return dp[nums.length-1]>dp[nums.length-2] ? dp[nums.length-1] : dp[nums.length-2];
+    }
+}
